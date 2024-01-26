cfcb4b5001bc870b6b15c4804c757e9d4b3a710b
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2024-01-26 07:18:33 +0100

    Changes for varaint2

--- scripts/components/Search.js
+++ scripts/components/Search.js
@@ -28,7 +28,7 @@ class Search {
   }
 
 
-    /**
+  /**
    * Extracts unique items from the given recipes.
    * @param {Array} filteredRecipes - The array of recipes to extract items from.
    * @returns {Array} - The array of unique items.
@@ -36,154 +36,187 @@ class Search {
   extractFilteredItems(filteredRecipes) {
     const filteredItems = [];
 
-    filteredRecipes.forEach(recipe => {
+    for (const recipe of filteredRecipes) {
       filteredItems.push(cleanString(recipe.appliance));
 
-      recipe.ustensils.forEach(ustensil => filteredItems.push(cleanString(ustensil)));
+      for (const ustensil of recipe.ustensils) {
+        filteredItems.push(cleanString(ustensil));
+      }
 
-      recipe.ingredients.forEach(ingredient => filteredItems.push(cleanString(ingredient.ingredient)));
-    });
+      for (const ingredient of recipe.ingredients) {
+        filteredItems.push(cleanString(ingredient.ingredient));
+      }
+    }
 
     return filteredItems;
   }
 
 
-    /**
+  /**
    * Updates the UI with the filtered recipes.
-   * It dynamically generates and displays recipe cards in the HTML document based on the filtered recipes. It also updates dropdown menus with the filtered items.
    * @param {Array} filteredRecipes - The array of recipes to display.
    */
   updateWithFilteredRecipes(filteredRecipes) {
 
     if (!filteredRecipes.length) {
       this.cardSection.innerHTML = "<p>Aucune recette n'a été trouvée.</p>";
-      this.numberOfRecipes.textContent = ``;
+      this.numberOfRecipes.textContent = '';
     } else {
-      this.cardSection.innerHTML = "";
-      this.numberOfRecipes.textContent = `${filteredRecipes.length} ${filteredRecipes.length === 1 ? 'recette' : 'recettes'}`;
+      this.cardSection.innerHTML = '';
+      this.numberOfRecipes.textContent = filteredRecipes.length + ' recettes';
 
-      filteredRecipes
-        .map(recipe => new Recipe(recipe))
-        .forEach(recipe => {
-          const templateCard = new RecipeCard(recipe);
-          templateCard.createCard();
-        });
-    };
+      for (let i = 0; i < filteredRecipes.length; i++) {
+        const recipe = new Recipe(filteredRecipes[i]);
+        const templateCard = new RecipeCard(recipe);
+        templateCard.createCard();
+      }
+    }
 
     const filteredItems = this.extractFilteredItems(filteredRecipes);
 
-    dropdowns.forEach(dropdown => dropdown.updateItems(filteredItems));
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].updateItems(filteredItems);
+    }
   }
 
 
-    /**
+  /**
    * Displays recipe cards in the UI.
    * @param {Array} recipes - The array of recipes to display.
    */
   displayRecipesCards(recipes) {
     this.cardSection.innerHTML = '';
-    recipes.forEach(recipe => new RecipeCard(recipe).createCard());
+
+    for (let i = 0; i < recipes.length; i++) {
+      const recipe = recipes[i];
+      new RecipeCard(recipe).createCard();
+    }
   }
 
 
   /**
-   *This method updates the currentRecipes array with the recipes provided in the filteredRecipes array. It clears the current content of currentRecipes using splice and replaces it with the content of filteredRecipes.
    * Updates the array of current recipes.
    * @param {Array} filteredRecipes - The array of recipes to set as current.
    */
-  updateCurrentRecipes = filteredRecipes => {
-  currentRecipes.splice(0,currentRecipes.length, ...filteredRecipes);
-  };
-
-
-    /**
-   * Sets up the search bar functionality.
-   * The searchBar method sets up event listeners for the search input and delete button, allowing the user to interact with the search functionality. 
-   * It dynamically updates the content based on the search input and selected tags.
-   */
-  searchBar() {
-    // Function to reset the content of the search
-    const resetContent = () => {
-      // Clear the displayed recipes
-      this.cardSection.innerHTML = '';
-      // Update the counter with the total number of recipes
-      this.numberOfRecipes.textContent = `${this.allRecipes.length} recipes`;
-      // Display all recipes in the card section
-      this.displayRecipesCards(this.allRecipes);
-      // Update the current recipes with all recipes
-      this.updateCurrentRecipes(this.allRecipes);
-      // Reset the items in all dropdowns
-      dropdowns.forEach(dropdown => dropdown.resetItemList());
-    };
-
-    // Function to update the content based on the search input
-    const updateContent = () => {
-      // Get the lowercase value of the search input
-      const searchInputValue = this.searchInput.value.toLowerCase();
-      // Show or hide the delete button based on the search input length
-      this.btnDelete.style.display = searchInputValue.length > 0 ? 'block' : 'none';
-
-      // If search input length is greater than 2, filter recipes
-      if (searchInputValue.length > 2) {
-        // Check if tags are selected, filter by tags, otherwise use all recipes
-        const recipesToFilter = this.selectedTags.length > 0 ? this.recipesFilteredByTag : this.allRecipes;
-        this.filterRecipesBySearch(recipesToFilter, searchInputValue);
-      }
-
-      // If search input is empty and tags are selected, filter by tags
-      else if (!this.searchInput.value && this.selectedTags.length > 0) {
-        this.filterRecipesByTags(this.allRecipes, this.selectedTags);
-      }
-
-      // If search input is empty and no tags selected, reset content
-      else if (!this.searchInput.value && this.selectedTags.length === 0) {
-        resetContent();
-      }
-    };
-
-    // Event listener for input changes to trigger content update
-    this.searchInput.addEventListener('input', updateContent);
-
-    // Event listener for delete button click to clear search input
-    this.btnDelete.addEventListener('click', () => {
-      // Clear search input value
-      this.searchInput.value = '';
-      // Hide the delete button
-      this.btnDelete.style.display = 'none';
-
-      // If tags are selected, filter by tags, otherwise reset content
-      if (this.selectedTags.length > 0) {
-        this.filterRecipesByTags(this.allRecipes, this.selectedTags);
-      } else if (this.selectedTags.length === 0) {
-        resetContent();
-      }
-    });
+  updateCurrentRecipes(filteredRecipes) {
+    currentRecipes.length = 0;
+    for (let i = 0; i < filteredRecipes.length; i++) {
+      currentRecipes.push(filteredRecipes[i]);
+    }
   }
 
+  /**
+   * Sets up the search bar functionality.
+   */
+searchBar() {
+  // Function to reset the content of the search
+  const resetContent = () => {
+    // Clear the displayed recipes
+    this.cardSection.innerHTML = '';
+    // Update the counter with the total number of recipes
+    this.numberOfRecipes.textContent = this.allRecipes.length + ' recettes';
+    // Display all recipes in the card section
+    this.displayRecipesCards(this.allRecipes);
+    // Update the current recipes with all recipes
+    this.updateCurrentRecipes(this.allRecipes);
+
+    // Reset the items in all dropdowns
+    for (let i = 0; i < dropdowns.length; i++) {
+      const dropdown = dropdowns[i];
+      dropdown.resetItemList();
+    }
+  };
+
+  // Function to update the content based on the search input
+  const updateContent = () => {
+    // Get the lowercase value of the search input
+    const searchInputValue = this.searchInput.value.toLowerCase();
+
+    // Show or hide the delete button based on the search input length
+    if (searchInputValue.length > 0) {
+      this.btnDelete.style.display = 'block';
+    } else {
+      this.btnDelete.style.display = 'none';
+    }
+
+    // If search input length is greater than 2, filter recipes
+    if (searchInputValue.length > 2) {
+      let recipesToFilter;
+      // Check if tags are selected, filter by tags, otherwise use all recipes
+      if (this.selectedTags.length > 0) {
+        recipesToFilter = this.recipesFilteredByTag;
+      } else {
+        recipesToFilter = this.allRecipes;
+      }
+      this.filterRecipesBySearch(recipesToFilter, searchInputValue);
+    }
+
+    // If search input is empty and tags are selected, filter by tags
+    else if (!this.searchInput.value && this.selectedTags.length > 0) {
+      this.filterRecipesByTags(this.allRecipes, this.selectedTags);
+    }
+    // If search input is empty and no tags selected, reset content
+    else if (!this.searchInput.value && this.selectedTags.length === 0) {
+      resetContent();
+    }
+  };
+
+  // Event listener for input changes to trigger content update
+  this.searchInput.addEventListener('input', updateContent);
+
+  // Event listener for delete button click to clear search input
+  this.btnDelete.addEventListener('click', () => {
+    // Clear search input value
+    this.searchInput.value = '';
+    // Hide the delete button
+    this.btnDelete.style.display = 'none';
+
+    // If tags are selected, filter by tags, otherwise reset content
+    if (this.selectedTags.length > 0) {
+      this.filterRecipesByTags(this.allRecipes, this.selectedTags);
+    } else if (this.selectedTags.length === 0) {
+      resetContent();
+    }
+  });
+}
 
 
-    /**
+
+  /**
    * Filters recipes based on the search input.
-   *  It normalizes the input value, applies the filter, and then updates the current recipes and the UI using the updateCurrentRecipes and updateWithFilteredRecipes methods.
    * @param {Array} recipes - The array of recipes to filter.
    * @param {string} inputValue - The search input value.
    */
   filterRecipesBySearch(recipes, inputValue) {
     const normalizedInputValue = cleanString(inputValue);
+    const filteredRecipes = [];
 
-    const filteredRecipes = recipes.filter(recipe => {
-      const {
-        description,
-        ingredients,
-        name
-      } = recipe;
+    for (let i = 0; i < recipes.length; i++) {
+      const recipe = recipes[i];
+      const { description, ingredients, name } = recipe;
 
-      return (
-        cleanString(description).includes(normalizedInputValue) ||
-        ingredients.some(ingredient => cleanString(ingredient.ingredient).includes(normalizedInputValue)) ||
-        cleanString(name).includes(normalizedInputValue)
-      );
-    });
+      let match = false;
+
+      if (cleanString(description).indexOf(normalizedInputValue) !== -1) {
+        match = true;
+      } else {
+        for (let j = 0; j < ingredients.length; j++) {
+          const ingredient = ingredients[j].ingredient;
+          if (cleanString(ingredient).indexOf(normalizedInputValue) !== -1) {
+            match = true;
+            break;
+          }
+        }
+      }
+
+      if (!match && cleanString(name).indexOf(normalizedInputValue) !== -1) {
+        match = true;
+      }
+
+      if (match) {
+        filteredRecipes.push(recipe);
+      }
+    }
 
     this.updateCurrentRecipes(filteredRecipes);
     this.updateWithFilteredRecipes(filteredRecipes);
@@ -192,36 +225,47 @@ class Search {
 
   /**
    * Filters recipes based on the selected tags.
-   * It normalizes the tags, applies the filter, and updates the current recipes and the UI.
    * @param {Array} recipes - The array of recipes to filter.
    * @param {Array} tags - The array of selected tags.
    */
   filterRecipesByTags(recipes, tags) {
-    const normalizedTags = tags.map(tag => cleanString(tag));
+    const normalizedTags = [];
+
+    for (let i = 0; i < tags.length; i++) {
+      normalizedTags.push(cleanString(tags[i]));
+    }
 
     if (normalizedTags.length === 0) {
-      // If no tags selected, reset the content
       this.updateCurrentRecipes(recipes);
       this.updateWithFilteredRecipes(recipes);
       return;
     }
-        // const filteredRecipes = this.allRecipes.filter(recipe => {
-    const filteredRecipes = recipes.filter(recipe => {
-      const {
-        appliance,
-        ustensils,
-        ingredients,
-        name
-      } = recipe;
 
-      return (
-        normalizedTags.every(tag =>
-          cleanString(appliance).includes(tag) ||
-          ustensils.some(ustensil => cleanString(ustensil).includes(tag)) ||
-          ingredients.some(ingredient => cleanString(ingredient.ingredient).includes(tag))
-        )
-      );
-    });
+    const filteredRecipes = [];
+
+    for (let i = 0; i < recipes.length; i++) {
+      const recipe = recipes[i];
+      const { appliance, ustensils, ingredients } = recipe;
+
+      let tagsMatch = true;
+
+      for (let j = 0; j < normalizedTags.length; j++) {
+        const tag = normalizedTags[j];
+
+        if (
+          cleanString(appliance).indexOf(tag) === -1 &&
+          !ustensils.some(ustensil => cleanString(ustensil).indexOf(tag) !== -1) &&
+          !ingredients.some(ingredient => cleanString(ingredient.ingredient).indexOf(tag) !== -1)
+        ) {
+          tagsMatch = false;
+          break;
+        }
+      }
+
+      if (tagsMatch) {
+        filteredRecipes.push(recipe);
+      }
+    }
 
     this.recipesFilteredByTag = filteredRecipes;
     this.updateCurrentRecipes(filteredRecipes);
@@ -230,7 +274,7 @@ class Search {
 
 
   /**
-   * Adds a tag to the selected tags, creates a corresponding UI element (Tag), and then filters recipes based on the updated set of selected tags.
+   * Adds a tag to the selected tags and filters recipes accordingly.
    * @param {string} tagText - The text of the tag to add.
    */
   addTag(tagText) {
@@ -238,50 +282,88 @@ class Search {
       const tag = new Tag(tagText, this);
       tag.createTag();
       this.selectedTags.push(tagText);
-      // this.filterRecipesByTags(this.allRecipes, this.selectedTags);
       this.filterRecipesByTags(currentRecipes, this.selectedTags);
     }
   }
 
+
   /**
    * Filters recipes based on tags, input value, or both.
-   * It checks for matches in the recipes array based on the normalized tags and input value. The filtered recipes are then updated using the updateCurrentRecipes method, and the UI is updated with the filtered recipes using the updateWithFilteredRecipes method.
    * @param {Array} recipes - The array of recipes to filter.
    * @param {Array} tags - The array of selected tags.
    * @param {string} inputValue - The search input value.
    */
-  filterRecipes = (recipes, tags, inputValue) => {
-
-      const normalizedTags = tags.map(tag => cleanString(tag));
+    filterRecipes = (recipes, tags, inputValue) => {
+      const normalizedTags = [];
+      for (let i = 0; i < tags.length; i++) {
+        normalizedTags.push(cleanString(tags[i]));
+      }
       const normalizedInputValue = cleanString(inputValue);
 
+      const filteredRecipes = [];
+      for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        const { appliance, ustensils, ingredients, name } = recipe;
 
-      const filteredRecipes = recipes.filter(recipe => {
-          const { appliance, ustensils, ingredients, name } = recipe;
+        let tagsMatch = normalizedTags.length === 0;
 
-          const tagsMatch = normalizedTags.length === 0 || normalizedTags.every(tag =>
-              [cleanString(appliance), ...ustensils.map(cleanString), ...ingredients.map(ingredient => cleanString(ingredient.ingredient))]
-              .some(item => item.includes(tag))
-          );
+        for (let j = 0; j < normalizedTags.length; j++) {
+          const tag = normalizedTags[j];
+          let tagIncluded = false;
 
-          const searchMatch = !normalizedInputValue || (
-              [cleanString(appliance), ...ustensils.map(cleanString), ...ingredients.map(ingredient => cleanString(ingredient.ingredient)), cleanString(name)]
-              .some(item => item.includes(normalizedInputValue))
-          );
+          const itemsToCheck = [cleanString(appliance)];
+          for (let k = 0; k < ustensils.length; k++) {
+            itemsToCheck.push(cleanString(ustensils[k]));
+          }
+          for (let k = 0; k < ingredients.length; k++) {
+            itemsToCheck.push(cleanString(ingredients[k].ingredient));
+          }
 
-          return tagsMatch && searchMatch;
-      });
+          for (let k = 0; k < itemsToCheck.length; k++) {
+            const item = itemsToCheck[k];
+            if (item.indexOf(tag) !== -1) {
+              tagIncluded = true;
+              break;
+            }
+          }
 
-      // this.updateCurrentRecipes(this.allRecipes);
-      // this.updateCurrentRecipes(currentRecipes);
+          if (!tagIncluded) {
+            tagsMatch = false;
+            break;
+          }
+        }
+
+        let searchMatch = !normalizedInputValue;
+
+        const itemsToCheck = [cleanString(appliance)];
+        for (let k = 0; k < ustensils.length; k++) {
+          itemsToCheck.push(cleanString(ustensils[k]));
+        }
+        for (let k = 0; k < ingredients.length; k++) {
+          itemsToCheck.push(cleanString(ingredients[k].ingredient));
+        }
+        itemsToCheck.push(cleanString(name));
+
+        for (let k = 0; k < itemsToCheck.length; k++) {
+          const item = itemsToCheck[k];
+          if (item.indexOf(normalizedInputValue) !== -1) {
+            searchMatch = true;
+            break;
+          }
+        }
+
+        if (tagsMatch && searchMatch) {
+          filteredRecipes.push(recipe);
+        }
+      }
+
       this.updateCurrentRecipes(filteredRecipes);
       this.updateWithFilteredRecipes(filteredRecipes);
-  };
+    };
 
 
   /**
    * Initializes the search functionality.
-   * It displays the initial set of recipe cards, sets up the search bar event listeners, and hides the delete button.
    */
   initialize() {
     this.displayRecipesCards(this.allRecipes);
